16d148352c63f99e5782d284aef4a310b53f5afc
sooyaaa0801 <72476689+sooyaaa0801@users.noreply.github.com>
2020-12-06 14:24:25 +0800

    changed line 18

--- Cheatsheet/Hello World.py
+++ Cheatsheet/Hello World.py
@@ -15,7 +15,7 @@ print("/___|\n")
 print("3. Variables")
 
 character_name = "John"
-character_age = "35"
+character_age = 35
 print("There once was a man named " + character_name + ",")
 print("he was " + character_age + " years old. ")
 print("He really liked the name " + character_name + ",")
